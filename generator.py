4b3a07155a7d64c8db001f2b7ef4d0b2f83d7916
crasherbe <87979743+crasherbe@users.noreply.github.com>
2026-03-07 01:19:54 +0700

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,10 +1,7 @@
 import random
-from strategy import HOT_PICK, COLD_PICK, NORMAL_PICK
+from strategy import HOT_PICK,COLD_PICK,NORMAL_PICK
 
 def generate_numbers(analysis, digit_count, total_count):
-    """
-    Generate kombinasi angka mengikuti strategi.
-    """
     hot = analysis["hot"]
     cold = analysis["cold"]
     normal = analysis["normal"]
@@ -12,23 +9,17 @@ def generate_numbers(analysis, digit_count, total_count):
     trend = analysis["trend"]
     
     numbers = []
-    
     for _ in range(total_count):
         combo = []
-        # ambil hot, cold, normal
         if len(hot) >= HOT_PICK:
-            combo += random.sample(hot, HOT_PICK)
+            combo += random.sample(hot,HOT_PICK)
         if len(cold) >= COLD_PICK:
-            combo += random.sample(cold, COLD_PICK)
+            combo += random.sample(cold,COLD_PICK)
         if len(normal) >= NORMAL_PICK:
-            combo += random.sample(normal, NORMAL_PICK)
-        
-        # lengkapi digit
+            combo += random.sample(normal,NORMAL_PICK)
         while len(combo) < digit_count:
             options = normal + trend + mirror
             combo.append(random.choice(options))
-        
         random.shuffle(combo)
         numbers.append("".join(combo))
-    
     return numbers
